7d48a24ccb25ee3a32604aa836fa70660641aae9
Logan <mightybug89@dyn-160-39-161-210.dyn.columbia.edu>
2015-10-13 23:04:55 -0400

    IT WORKED
    HAHAHAHAHAHH

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Response
 
 import json
 import time
@@ -8,8 +9,19 @@ import sys
 import random
 
 import pyorient
+from Queue import Queue
 
 app = Flask(__name__)
+q = Queue()
+
+def event_stream():
+    while True:
+        result = q.get()
+        yield 'data: %s\n\n' % str(result)
+        
+@app.route('/eventSource/')
+def sse_source():
+    return Response( event_stream(), mimetype='text/event-stream' )
 
 @app.route("/")
 def index():
@@ -18,6 +30,7 @@ def index():
 @app.route("/getData/")
 def getData():
 
+	q.put("starting data query...")
 	lat1 = str(request.args.get('lat1'))
 	lng1 = str(request.args.get('lng1'))
 	lat2 = str(request.args.get('lat2'))
@@ -26,7 +39,7 @@ def getData():
 	print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 	
 	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "password")
+	session_id = client.connect("root", "gringoThunder")
 	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
@@ -61,6 +74,7 @@ def getData():
 
 		output["features"].append(feature)
 
+	q.put('idle')
 	return json.dumps(output)
 
 if __name__ == "__main__":
